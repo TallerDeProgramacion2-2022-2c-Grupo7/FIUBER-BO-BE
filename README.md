3f58bb5e5c56f19e23fee4bb67ac1561a805ca62
Guido Bergman <gbergman@fi.uba.ar>
2022-09-13 11:07:20 -0300

    Add test

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # FIUBER-BO-BE
 Backend side of FIUBER’s back office
+
+
+## Instalar dependencias
+``` bash
+poetry install
+```
+--- 
+
+## Ejecución con Docker
+
+``` bash
+docker build -t fastapi-app .
+docker container run -it -p 8000:8000 fastapi-app
+```
+
+---
+
+## Correr tests
+``` bash
+poetry run pytest
+```
+
--- tests/test_fastapi_app.py
+++ tests/test_fastapi_app.py
@@ -1,5 +1,15 @@
-from fastapi_app import __version__
+from fastapi.testclient import TestClient
 
+from pathlib import Path
+import sys
+path = str(Path(Path(__file__).parent.absolute()).parent.absolute())
+sys.path.insert(0, path + "/fastapi_app")
 
-def test_version():
-    assert __version__ == '0.1.0'
+from app import app
+
+client = TestClient(app)
+
+def test_hola_mundo():
+    response = client.get("/hola")
+    assert response.status_code == 200
+    assert response.json() == "Hola mundo!"
